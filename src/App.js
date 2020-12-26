4639ccb1d54b1ace036cf62047cdbdab4f835a39
mbmcmullen27 <mbmcmullen27@gmail.com>
2020-12-26 10:29:14 -0600

    Snackback as a full comonent

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import './App.css';
+import Snackbar from './Snackbar.jsx'
 
-//var mqtt = require('mqtt');
-import Paho from 'paho-mqtt';
-
-var client  = new Paho.Client('localhost', 8883, 'glub01')
-
-// connect the client
-client.connect({onSuccess:onConnect});
-client.onConnectionLost = onConnectionLost;
-
-
-// called when the client connects
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  client.subscribe("Glubs");
-}
-
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
-}
 
 function App() {
-  var note;
-  client.onMessageArrived = function(message) {
-    note = message.payloadString;
-    setMesg(note);
-    console.log(note);
-  };
-
-  const [mesg, setMesg] = useState(<Fragment><em>nothing glubbed...</em></Fragment>);
   return (
     <div className="App">
-      <p>message: {mesg}</p>
+      <Snackbar />
       <video id="video" autoPlay muted loop>
         <source type="video/mp4" src="https://video.twimg.com/tweet_video/En0CdryWMAE7nTK.mp4"/>
-      </video>
+      </video>      
     </div>
   );
 }
---
+++ src/Snackbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './App.css';
+
+import Paho from 'paho-mqtt';
+
+
+export default class SnackBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            note: '',
+            client: new Paho.Client('localhost', 8883, 'glub01')
+        }
+
+       this.onConnect = this.onConnect.bind(this);
+       this.onConnectionLost = this.onConnectionLost.bind(this);
+       this.onMessageArrived = this.onMessageArrived.bind(this);
+        
+    }
+
+    componentDidMount(){
+        var { client } = this.state;
+        client.connect({onSuccess:this.onConnect});
+        client.onConnectionLost = this.onConnectionLost;
+        client.onMessageArrived = this.onMessageArrived;
+    }
+
+    onConnect() {
+        var { client } = this.state;
+        client.subscribe("Glubs");
+    }
+  
+    onConnectionLost(responseObject) {
+        if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:"+responseObject.errorMessage);
+        }
+    }
+
+    onMessageArrived (message) {
+        this.setState({note: message.payloadString})
+        console.log(message);
+        var toast = document.getElementById("snackbar");
+        toast.className = "show";
+    
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);;
+    
+      };
+    
+    render(){
+        const { note } = this.state
+        return (
+            <div id="snackbar">{ note }</div>
+        );
+    }
+}
